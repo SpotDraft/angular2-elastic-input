294719ca47927b0f80f7bb47bdb4ae9ff4ba00f8
Anirudh Varma <anirudh@spotdraft.com>
2020-07-10 17:13:38 +0530

    Add generic template to forRoot type

--- src/elastic-input.module.ts
+++ src/elastic-input.module.ts
@@ -1,14 +1,14 @@
-import {NgModule, ModuleWithProviders} from '@angular/core';
-import {ElasticInputDirective} from './elastic-input.directive';
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { ElasticInputDirective } from "./elastic-input.directive";
 
 @NgModule({
   declarations: [ElasticInputDirective],
-  exports: [ElasticInputDirective]
+  exports: [ElasticInputDirective],
 })
 export class ElasticInputModule {
-  static forRoot (): ModuleWithProviders {
+  static forRoot(): ModuleWithProviders<ElasticInputModule> {
     return {
-      ngModule: ElasticInputModule
+      ngModule: ElasticInputModule,
     };
   }
 }
